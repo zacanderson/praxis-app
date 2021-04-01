7860c4e0cda2aef696bdf012c55e5ca07b4c9388
Alex Mayorga <alexmayorga@knights.ucf.edu>
2021-03-31 21:16:29 -0400

    Adds catch exception for testing

--- api/passwordreset.js
+++ api/passwordreset.js
@@ -11,26 +11,34 @@ exports.setApp = function (app, client) {
 
 		//get incoming json data
 		const { accessToken, UserID, Password } = req.body;
-		const objID = mongoose.Types.ObjectId(UserID);
-
-		//get verification list by id
-		const verifyList = await Verify.find({ UserID: UserID });
-
-		var error = '';
-
-		//if the verify list isnt open and the tokens match then change password
-		if(verifyList.length > 0 && bcrypt.compare(accessToken, verifyList[0].accessToken)) {
-			//change passwords and update 
-			const hash = await bcrypt.hash(Password, Number(process.env.SALT_ROUNDS));
-			await Users.updateOne({ _id: objID }, {$set: { Password: hash } });
+		let objID, error;
+		try {
+			objID = mongoose.Types.ObjectId(UserID);
 			
-			//delete from Verify collection
-			Verify.deleteMany({ UserID: UserID });
+			//get verification list by id
+			const verifyList = await Verify.find({ UserID: UserID });
 
-		} else {
-			error = 'access error';
+			//if the verify list isnt open and the tokens match then change password
+			if(verifyList.length > 0 && bcrypt.compare(accessToken, verifyList[0].accessToken)) {
+				//change passwords and update 
+				const hash = await bcrypt.hash(Password, Number(process.env.SALT_ROUNDS));
+				await Users.updateOne({ _id: objID }, {$set: { Password: hash } });
+				
+				//delete from Verify collection
+				Verify.deleteMany({ UserID: UserID });
 
+			} else {
+				error = 'access error';
+
+			}
+
+		} catch (err){
+			error = 'ObjectID parsing error';
+			
 		}
+		
+
+		
 
 		const ret = { error: error }; 
 
