51e0aa3eb1209f1935cbfc18d158ebad3f7010da
tristanjp12 <tristanjaramillopritchard@gmail.com>
2021-04-05 17:09:47 -0400

    AddHabit now works with the Habit array

--- api/addHabit.js
+++ api/addHabit.js
@@ -2,34 +2,29 @@
 require('express');
 require('mongodb');
 require('dotenv').config();
-const Habit = require('../models/Habits.js');
-
-exports.setApp = function (app, client) {
+const Users = require('../models/Users.js');
+const ObjectId = require('mongodb').ObjectID;
+const webT = require('jsonwebtoken');
+const jwt = require("../createJWT");
+exports.setApp = function (app, client) 
+{
 
 	app.post('/api/addHabit', async (req, res, next) => {
 
 		var error = '';
 
 		//get habit name
-		const {habit, UserID} = req.body;	
+		const {habit, accessToken, userID} = req.body;	
 
-		const newHabit = new Habit({Habitname:habit, UserId:UserID});
-
-
-		try
-		{
-			//const db = client.db();
-			//const result = db.collection('Habits').insertOne(newHabit);
-			newHabit.save();
-
-
+		if(jwt.isExpired(accessToken)){
+			error = 'Acess token has expired, log back in';
+		}else{
+				let userData = webT.decode(accessToken, { complete: true });
+				let userID = userData.payload.userID;
+				await Users.updateOne({ _id: ObjectId(userID) }, { $push: { Habits: habit} });
+				ret = jwt.refreshToken(accessToken);
 		}
-		catch(e)
-		{
-			error = e.toString();
-		}
-
-		var ret = {error:error}
+		ret.error = error;
 		res.status(200).json(ret);
 
 	});
